0678fab6413bba15cc08058aa0ca94871e6245b8
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-02-08 22:53:03 -0300

    corrected misstypo

--- src/modules/tickets/repositories/ITicketsRepository.ts
+++ src/modules/tickets/repositories/ITicketsRepository.ts
@@ -5,5 +5,5 @@ export default interface ITicketsRepository {
   save(ticket: Ticket): Promise<Ticket>;
   findByID(id: string, relations?: string[]): Promise<Ticket | undefined>;
   fetchByTicketID(id: string): Promise<Ticket>;
-  fetchValidateTicket(ticket_id: string): Promise<Boolean>;
+  fetchValidateTicket(ticket_id: string): Promise<boolean>;
 }
