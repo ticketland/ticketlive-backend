675d31b0e28a590c801af4ea7c30eec145cdb758
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-03-15 21:33:40 -0300

    refactor: dTO to create entrace following new DB

--- src/modules/entrance/dtos/ICreateEntranceDTO.ts
+++
@@ -1,6 +0,0 @@
-export default interface ICreateEntraceDTO {
-  ingresso_id: string;
-  evento_id: string;
-  usuario_id: string;
-  participante_id: string;
-}
---
+++ src/modules/entrances/dtos/ICreateEntranceDTO.ts
@@ -0,0 +1,6 @@
+export default interface ICreateEntraceDTO {
+  user_id: string;
+  ticket_id: string;
+  ext_event_id: number;
+  ext_participant_id: number;
+}
