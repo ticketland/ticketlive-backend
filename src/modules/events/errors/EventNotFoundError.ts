c5dec165dd0a6fcca115fe7bb9ad9d4c2c8a5b0a
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-03-15 21:40:53 -0300

    refactor: remove unused file

--- src/modules/events/errors/EventNotFoundError.ts
+++
@@ -1,9 +0,0 @@
-import AppError from '@shared/errors/AppError';
-
-class EventNotFoundError extends AppError {
-  constructor(message = 'Event not found!', statusCode = 404) {
-    super(message, statusCode);
-  }
-}
-
-export default EventNotFoundError;
