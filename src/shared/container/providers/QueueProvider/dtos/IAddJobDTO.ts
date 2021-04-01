854fce8daeb933459c3ea1bf65378b77bd8e0110
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-04-01 11:56:36 -0300

    removed queue provider

--- src/shared/container/providers/QueueProvider/dtos/IAddJobDTO.ts
+++
@@ -1,7 +0,0 @@
-import { JobOptions } from 'bull';
-
-export default interface IAddJob {
-  name: string;
-  data?: any;
-  options?: JobOptions;
-}
--- src/shared/container/providers/QueueProvider/dtos/IScheduleJobDTO.ts
+++
@@ -1,8 +0,0 @@
-import { JobOptions } from 'bull';
-
-export default interface IScheduleJobDTO {
-  name: string;
-  date: number | string;
-  data?: any;
-  options?: JobOptions;
-}
