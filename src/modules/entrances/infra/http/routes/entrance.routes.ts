69a7aac82b6ab6ae82fc66bb97a1347b05c9eda7
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-04-01 13:51:22 -0300

    chore: add fully support to eslint

--- src/modules/entrances/infra/http/routes/entrance.routes.ts
+++ src/modules/entrances/infra/http/routes/entrance.routes.ts
@@ -1,16 +1,12 @@
-import { Router } from 'express';
-
-// Contollers
-import CreateEntranceController from '@modules/entrances/useCases/createEntrance/CreateEntranceController'
-import FindEntranceController from '@modules/entrances/useCases/findEntrance/FindEntranceController'
-
-
+import CreateEntranceController from "@modules/entrances/useCases/createEntrance/CreateEntranceController";
+import FindEntranceController from "@modules/entrances/useCases/findEntrance/FindEntranceController";
+import { Router } from "express";
 
 const entranceRouter = Router();
 const createEntranceController = new CreateEntranceController();
 const findEntranceController = new FindEntranceController();
 
-entranceRouter.post('/', createEntranceController.handle);
-entranceRouter.get('/:entrance_id', findEntranceController.handle);
+entranceRouter.post("/", createEntranceController.handle);
+entranceRouter.get("/:entrance_id", findEntranceController.handle);
 
 export default entranceRouter;
