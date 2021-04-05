c5f1fee6a51b321379186d34e155ee628a4a6408
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-04-05 18:02:06 -0300

    refactor: rename class

--- src/modules/entrances/repositories/IEntrancesRepository.ts
+++ src/modules/entrances/repositories/IEntrancesRepository.ts
@@ -1,7 +1,7 @@
 import ICreateEntraceDTO from '@modules/entrances/dtos/ICreateEntranceDTO';
 import Entrance from '@modules/entrances/infra/typeorm/entities/Entrance';
 
-export default interface IUsersRepository {
+export default interface IEntrancesRepository {
   create(entraceData: ICreateEntraceDTO): Promise<Entrance>;
   findByTicketID(ticket_id: string): Promise<Entrance | undefined>;
   findByEventID(ext_event_id: string): Promise<Entrance[]>;
