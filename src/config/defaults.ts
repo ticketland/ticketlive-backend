89056d92c7e3e60ef17af5d80fb9e7438cf858ab
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-04-28 10:51:17 -0300

    fix: fix a bug where defaults where hardcoded and not going to the .env file

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -4,12 +4,17 @@ const paginationDefaults = {
 };
 
 const paymentMethodsDefaults = {
-  money_payment_method_id: '4a8d0740-48cf-4983-9e04-ecaf21237db1',
+  money_payment_method_id:
+    process.env.MONEY_PAYMENT_METHOD_ID ||
+    '4a8d0740-48cf-4983-9e04-ecaf21237db1',
 };
 
 const operationsDefaults = {
-  withdrawal_operation_id: '23e9a016-0dce-4ecb-b830-18523f687914',
-  sale_operation_id: 'b422742b-0041-477c-a8db-071585570826',
+  withdrawal_operation_id:
+    process.env.WITHDRAWAL_OPERATION_ID ||
+    '23e9a016-0dce-4ecb-b830-18523f687914',
+  sale_operation_id:
+    process.env.SALE_OPERATION_ID || 'b422742b-0041-477c-a8db-071585570826',
 };
 
 export { paginationDefaults, paymentMethodsDefaults, operationsDefaults };
